9961bd3d26052aa9e04fff80216580be2314f7b3
buzz <github.20.buz_01@spamgourmet.com>
2015-11-17 20:36:06 +0100

    Reformat code / comment unnecessary prints / add test for date check

--- test/unit/service/test_processor.py
+++ test/unit/service/test_processor.py
@@ -1,5 +1,5 @@
 import locale
-from datetime import datetime,date,timedelta
+from datetime import datetime, date
 from unittest import TestCase
 
 from model.search import Search
@@ -34,6 +34,7 @@ class TestProcessor(TestCase):
         self.assertEquals(len(l), Processor.nb_ad_per_page * nb_page)
 
     def test_get_item_list_with_stop_item(self):
+        """ Test the process with a stop item taken from a search"""
         s = Search(search_id=999, label="Test Search",
                    url="http://www.leboncoin.fr/materiel_medical/offres/ile_de_france/occasions/?f=a&th=1&location=Blois%2041000")
         p = Processor(s)
@@ -50,11 +51,15 @@ class TestProcessor(TestCase):
 
     def test_parse_date(self):
         locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
-        print (date.today().strftime('%e %b %Y'))
-        for i in range(30,360,30):
-            print (date.today() - timedelta(i)).strftime('%e %b %Y')
+        # print (date.today().strftime('%e %b %Y'))
+        # for i in range(30, 360, 30):
+        #     print (date.today() - timedelta(i)).strftime('%e %b %Y')
 
-        self.assertEquals(Processor.parse_date('28 Oct', '11:00'), datetime.strptime(date.today().strftime('%Y')+'/10/28 11:00',"%Y/%m/%d %H:%M"))
-        self.assertEquals(Processor.parse_date('1 oct', '14:27'), datetime.strptime(date.today().strftime('%Y')+'/10/01 14:27',"%Y/%m/%d %H:%M"))
-        self.assertEquals(Processor.parse_date('13 sept', '08:15'), datetime.strptime(date.today().strftime('%Y')+'/09/13 08:15',"%Y/%m/%d %H:%M"))
-
+        self.assertEquals(Processor.parse_date('16 nov', '11:00'),
+                          datetime.strptime(date.today().strftime('%Y') + '/11/16 11:00', "%Y/%m/%d %H:%M"))
+        self.assertEquals(Processor.parse_date('28 Oct', '11:00'),
+                          datetime.strptime(date.today().strftime('%Y') + '/10/28 11:00', "%Y/%m/%d %H:%M"))
+        self.assertEquals(Processor.parse_date('1 oct', '14:27'),
+                          datetime.strptime(date.today().strftime('%Y') + '/10/01 14:27', "%Y/%m/%d %H:%M"))
+        self.assertEquals(Processor.parse_date('13 sept', '08:15'),
+                          datetime.strptime(date.today().strftime('%Y') + '/09/13 08:15', "%Y/%m/%d %H:%M"))
